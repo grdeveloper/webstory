97b7d05c36df6d1593a97c10ae80b97204b83fc9
GrProgrammer <joepetrosyan@mail.ru>
2019-04-28 01:43:52 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,117 +1,119 @@
-(function(window, document){
-'use strict';
+/**
 
-    var slideIndex = 0;
-    showSlides();
+All the varibales are set on the global object.
+This is just for demo porposes only.
 
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}
-        slides[slideIndex-1].style.display = "block";
-        setTimeout(showSlides, 5000);
+*/
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000);
+}
 
-    var rotateImages = document.querySelectorAll('.description img');
+var rotateImages = document.querySelectorAll('.description img');
+for(var i = 0; i < rotateImages.length; i++){
+    rotateImages[i].style.transform = 'rotate(-180deg)';
+}
+
+function scrolled() {
     for(var i = 0; i < rotateImages.length; i++){
-        rotateImages[i].style.transform = 'rotate(-180deg)';
+        rotateImages[i].style.transform = 'rotate('+(-180 + window.pageYOffset/2 + i*15)+'deg)';
+    }
+}
+
+var scr = document.getElementsByClassName('scroll')[0];
+scr.style.opacity = "0";
+
+
+window.addEventListener('scroll', scrumb);
+window.addEventListener('scroll', scrolled);
+scr.addEventListener('click', function () {
+    window.scrollTo({"left": "0", "top" : "0", "behavior" : "smooth"});
+});
+
+function scrumb() {
+    var foot = document.querySelector('.foot').offsetTop;
+    if(window.pageYOffset >0){
+        scr.style.opacity = "1";
+    }else{
+        scr.style.opacity = "0";
     }
 
-    function scrolled() {
-        for(var i = 0; i < rotateImages.length; i++){
-            rotateImages[i].style.transform = 'rotate('+(-180 + window.pageYOffset/2 + i*15)+'deg)';
-        }
+    if(window.pageYOffset > foot - 636){
+        scr.style.borderColor = "white";
+        scr.style.background = 'white';
+    }else{
+        scr.style.borderColor = "";
+        scr.style.background = "";
     }
+}
 
-    var scr = document.getElementsByClassName('scroll')[0];
-    scr.style.opacity = "0";
+var collapse = document.querySelector('.collapse');
+var header = document.querySelector('header');
 
+collapse.addEventListener('click', function () {
+    header.classList.toggle('clicked');
+    collapse.classList.toggle('changed');
+});
 
-    window.addEventListener('scroll', scrumb);
-    window.addEventListener('scroll', scrolled);
-    scr.addEventListener('click', function () {
-        window.scrollTo({"left": "0", "top" : "0", "behavior" : "smooth"});
-    });
+var d = document.querySelector(".map");
 
-    function scrumb() {
-        var foot = document.querySelector('.foot').offsetTop;
-        if(window.pageYOffset >0){
-            scr.style.opacity = "1";
-        }else{
-            scr.style.opacity = "0";
-        }
+var options = {
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
 
-        if(window.pageYOffset > foot - 636){
-            scr.style.borderColor = "white";
-            scr.style.background = 'white';
-        }else{
-            scr.style.borderColor = "";
-            scr.style.background = "";
-        }
-    }
+var map = new google.maps.Map(d, options);
 
-    var collapse = document.querySelector('.collapse');
-    var header = document.querySelector('header');
+function success(position) {
+    var loc = new google.maps.LatLng(position.coords.latitude,
+        position.coords.longitude);
+    map.setCenter(loc);
+    var label = new google.maps.InfoWindow();
+    label.setContent("You are here!");
+    label.setPosition(loc);
+    label.open(map);
+}
 
-    collapse.addEventListener('click', function () {
-        header.classList.toggle('clicked');
-        collapse.classList.toggle('changed');
-    });
-
-    var d = document.querySelector(".map");
-
+function initMap() {
     var options = {
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 8,
+        center: {lat: 40.151864, lng: 44.483192}
     };
-
     var map = new google.maps.Map(d, options);
+}
 
-    function success(position) {
-        var loc = new google.maps.LatLng(position.coords.latitude,
-            position.coords.longitude);
-        map.setCenter(loc);
-        var label = new google.maps.InfoWindow();
-        label.setContent("You are here!");
-        label.setPosition(loc);
-        label.open(map);
-    }
+function error() {
+    initMap();
+}
 
-    function initMap() {
-        var options = {
-            zoom: 8,
-            center: {lat: 40.151864, lng: 44.483192}
-        };
-        var map = new google.maps.Map(d, options);
-    }
+params = {
+    enableHighAccuracy: true,
+    timeout: 10000
+};
 
-    function error() {
-        initMap();
-    }
-
-    params = {
-        enableHighAccuracy: true,
-        timeout: 10000
-    };
-
-    navigator
-        .geolocation
-        .getCurrentPosition(success, error, params);
+navigator
+    .geolocation
+    .getCurrentPosition(success, error, params);
 
 
 
-    // var geocoder = new google.maps.Geocoder();
-    // geocoder.geocode({
-    //     "address": inputAddress
-    // }, function(results) {
-    //     console.log(results[0].geometry.location); //LatLng
-    // });
-    
-})(window, document);
+// var geocoder = new google.maps.Geocoder();
+// geocoder.geocode({
+//     "address": inputAddress
+// }, function(results) {
+//     console.log(results[0].geometry.location); //LatLng
+// });
 
 
 
